417869c31240654d8d42283dd9cf5856676ff90a
LEEJUNJAE <46310910+eazuooz@users.noreply.github.com>
2026-01-16 19:44:49 +0900

    Add Ray class and update project files
    Introduced the Ray class in Ray.h for ray tracing functionality. Updated the Visual Studio project and filters files to include Ray.h in the Common filter group.

---
+++ RayTracinginOneWeekendinCUDA/RayTracinginOneWeekend/Ray.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef RAY_H
+#define RAY_H
+
+#include "Vec3.h"
+
+class Ray
+{
+public:
+    Ray() {}
+
+    Ray(const Point3& origin, const Vec3& direction) 
+        : orig(origin), dir(direction) {}
+
+    const Point3& Origin() const { return orig; }
+    const Vec3& Direction() const { return dir; }
+
+    Point3 At(double t) const
+    {
+        return orig + t * dir;
+    }
+
+private:
+    Point3 orig;
+    Vec3 dir;
+};
+
+#endif
