be40162e206f6c64bd4c4715b39ce3a872ce01f5
Jay <46310910+eazuooz@users.noreply.github.com>
2026-01-19 17:16:31 +0900

    Update RtWeekend.h

--- RayTracinginOneWeekendinCUDA/RayTracinginOneWeekend/RtWeekend.h
+++ RayTracinginOneWeekendinCUDA/RayTracinginOneWeekend/RtWeekend.h
@@ -2,9 +2,11 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Constants
 
@@ -18,8 +20,26 @@ inline double DegreesToRadians(double degrees)
     return degrees * Pi / 180.0;
 }
 
-// Common Headers
+inline double DegreesToRadians(double degrees)
+{
+    return degrees * Pi / 180.0;
+}
 
+inline double RandomDouble()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double RandomDouble(double minimum, double maximum)
+{
+    // Returns a random real in [minimum, maximum)
+    return minimum + (maximum - minimum) * RandomDouble();
+}
+
+
+// Common Headers
 #include "Color.h"
 #include "Interval.h"
 #include "Ray.h"
