f820d35d03002957fc446b3e286867dc34cd3ed8
LEEJUNJAE <46310910+eazuooz@users.noreply.github.com>
2026-01-20 23:02:22 +0900

    Update scene generation and camera settings
    Replaces the static scene with a randomized scene featuring a ground sphere and many small spheres with varying materials. Updates camera parameters for a wider view, higher resolution, more samples per pixel, and depth of field adjustments to improve image quality and scene complexity.

--- RayTracinginOneWeekendinCUDA/RayTracinginOneWeekend/kernel.cu
+++ RayTracinginOneWeekendinCUDA/RayTracinginOneWeekend/kernel.cu
@@ -64,39 +64,69 @@ int main()
 
 	HittableList world;
 
-	auto R = std::cos(Pi / 4);
+	auto groundMaterial = std::make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
+	world.Add(std::make_shared<Sphere>(Point3(0.0, -1000.0, 0.0), 1000.0, groundMaterial));
 
-	//auto materialLeft = std::make_shared<Lambertian>(Color(0, 0, 1));
-	//auto materialRight = std::make_shared<Lambertian>(Color(1, 0, 0));
+	for (int gridX = -11; gridX < 11; gridX++)
+	{
+		for (int gridZ = -11; gridZ < 11; gridZ++)
+		{
+			const double materialSelector = RandomDouble();
 
-	//world.Add(std::make_shared<Sphere>(Point3(-R, 0, -1), R, materialLeft));
-	//world.Add(std::make_shared<Sphere>(Point3(R, 0, -1), R, materialRight));
+			const Point3 center(
+				gridX + 0.9 * RandomDouble(),
+				0.2,
+				gridZ + 0.9 * RandomDouble()
+			);
 
-	auto materialGround = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-	auto materialCenter = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-	auto materialLeft = std::make_shared<Dielectric>(1.50);
-	auto materialBubble = std::make_shared<Dielectric>(1.0 / 1.50);
-	auto materialRight = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
+			if ((center - Point3(4.0, 0.2, 0.0)).Length() > 0.9)
+			{
+				std::shared_ptr<Material> sphereMaterial;
 
-	world.Add(std::make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, materialGround));
-	world.Add(std::make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, materialCenter));
-	world.Add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-	world.Add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.4, materialBubble));
-	world.Add(std::make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, materialRight));
+				if (materialSelector < 0.8)
+				{
+					const Color albedo = Color::Random() * Color::Random();
+					sphereMaterial = std::make_shared<Lambertian>(albedo);
+				}
+				else if (materialSelector < 0.95)
+				{
+					const Color albedo = Color::Random(0.5, 1.0);
+					const double fuzz = RandomDouble(0.0, 0.5);
+					sphereMaterial = std::make_shared<Metal>(albedo, fuzz);
+				}
+				else
+				{
+					sphereMaterial = std::make_shared<Dielectric>(1.5);
+				}
+
+				world.Add(std::make_shared<Sphere>(center, 0.2, sphereMaterial));
+			}
+		}
+	}
+
+	auto material1 = std::make_shared<Dielectric>(1.5);
+	world.Add(std::make_shared<Sphere>(Point3(0.0, 1.0, 0.0), 1.0, material1));
+
+	auto material2 = std::make_shared<Lambertian>(Color(0.4, 0.2, 0.1));
+	world.Add(std::make_shared<Sphere>(Point3(-4.0, 1.0, 0.0), 1.0, material2));
+
+	auto material3 = std::make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0);
+	world.Add(std::make_shared<Sphere>(Point3(4.0, 1.0, 0.0), 1.0, material3));
 
 	Camera camera;
 
 	camera.aspectRatio = 16.0 / 9.0;
-	camera.imageWidth = 400;
-	camera.samplesPerPixel = 100;
+	camera.imageWidth = 1200;
+	camera.samplesPerPixel = 500;
 	camera.maxDepth = 50;
-	camera.vfov = 20;
-	camera.lookfrom = Point3(-2, 2, 1);
-	camera.lookat = Point3(0, 0, -1);
-	camera.vup = Vec3(0, 1, 0);
 
-	camera.defocusAngle = 10.0;
-	camera.focusDistance = 3.4;
+	camera.vfov = 20.0;
+	camera.lookfrom = Point3(13.0, 2.0, 3.0);
+	camera.lookat = Point3(0.0, 0.0, 0.0);
+	camera.vup = Vec3(0.0, 1.0, 0.0);
+
+	camera.defocusAngle = 0.6;
+	camera.focusDistance = 10.0;
 
 	camera.Render(world);
 
