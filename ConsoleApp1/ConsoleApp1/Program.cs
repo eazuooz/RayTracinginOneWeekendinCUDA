123ee59115af9afa8515c224ce77aedfed5d42f0
LEEJUNJAE <46310910+eazuooz@users.noreply.github.com>
2026-08-29 14:49:00 +0900

    Add console and WPF app scaffolds
    Introduce new .NET 10 console and WPF starter projects, including solution files, project definitions, and initial UI/code-behind wiring.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿//onsole.WriteLine("Hello, World!");
+
+
+
+int[,] matrix = new int[3, 3]
+{
+    { 0, 0, 0 },
+    { 0, 0, 0 },
+    { 0, 0, 0 }
+};
+
+
+matrix[0, 2] = 3;
+
+for (int i = 0; i < 3; i++)
+{
+    //
+}
+///
+
+if (matrix.Length == 0)
+{
+    //
+}
+
+// 게임 아예 못만든다.
+
+// 
